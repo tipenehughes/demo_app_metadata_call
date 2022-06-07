4eb313233c846ba8c7a3e941e4fb2951d87a5382
tipenehughes <tipenehughes@gmail.com>
2022-06-07 15:29:37 -0400

    updated syntax

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-let client = ZAFClient.init();
+const client = ZAFClient.init();
 client.invoke("resize", { width: "100%", height: "300px" });
 
 // Don't know app's Installation ID yet.
@@ -10,39 +10,39 @@ const formName = document.getElementById("formName");
 // Checkbox input
 const myCheckbox = document.getElementById("myCheckbox");
 
-function getMetaData() {
-	client.metadata().then(function (metadata) {
-		console.log("metadata:", metadata);
+async function getMetaData() {
+	const metadata = await client.metadata();
+	console.log("metadata:", metadata);
 
-		// Need Installation ID when saving later.
-		console.log("installationId:", metadata["installationId"]);
-		installationId = metadata["installationId"];
+	// Need Installation ID when saving later.
+	installationId = metadata["installationId"];
+	console.log("installationId:", metadata["installationId"]);
 
-		// Get and display app setting called "form_name" of type "text"
-		console.log("form_name:", metadata.settings["form_name"]);
-		formName.value = metadata.settings["form_name"];
+	// Get and display app setting called "form_name" of type "text"
+	formName.value = metadata.settings["form_name"];
+	console.log("form_name:", metadata.settings["form_name"]);
 
-		// Get and display app setting called "enable_role_restriction" of type "checkbox"
-		let role_restriciton = metadata.settings["enable_role_restriction"];
-		console.log("enable_role_restriction:", role_restriciton);
-		if (role_restriciton == true) {
-			return (myCheckbox.checked = true);
-		} else {
-			return (myCheckbox.checked = false);
-		}
-	});
+	// Get and display app setting called "enable_role_restriction" of type "checkbox"
+	let role_restriciton = metadata.settings["enable_role_restriction"];
+	console.log("enable_role_restriction:", role_restriciton);
+
+	// Set state of checkbox in the app UI
+	if (role_restriciton == true) {
+		return (myCheckbox.checked = true);
+	} else {
+		return (myCheckbox.checked = false);
+	}
 }
 
-function setMetaData() {
-	console.log(installationId);
+async function setMetaData() {
 
 	// Get new values from user for form_name and enable_role_restriction app setting fields.
-	let newFormName = formName.value;
-	let newCheckboxValue = myCheckbox.checked;
-	let data = { settings: { form_name: newFormName, enable_role_restriction: newCheckboxValue } };
+	const newFormName = formName.value;
+	const newCheckboxValue = myCheckbox.checked;
+	const data = { settings: { form_name: newFormName, enable_role_restriction: newCheckboxValue } };
 	console.log("data:", data);
 
-	let updateSettings = {
+	const updateSettingsConfig = {
 		url: `/api/v2/apps/installations/${installationId}.json`,
 		type: "PUT",
 		contentType: "application/json",
@@ -50,9 +50,7 @@ function setMetaData() {
 		dataType: "json",
 	};
 
-	// Update app settings.
-	client
-		.request(updateSettings)
-		.then((success) => console.log("success:", success))
-		.catch((error) => console.log("error:", error));
+	// Update app settings and display result.
+	const updateSettings = await client.request(updateSettingsConfig);
+	console.log("Result:", updateSettings);
 }
